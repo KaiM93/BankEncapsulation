ba7bbb59b358cecca4136a141b5479ca1a0f7fbb
Kai Morris <kaij.morris@yahoo.com>
2022-10-28 13:30:51 -0400

    finished exercise

---
+++ BankEncapsulation/Bank Account.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace BankEncapsulation
+{
+    public class Bank_Account
+    {
+        public Bank_Account()
+        {
+        }
+
+        private double balance = 0;
+
+        public void Deposit(double amount)
+        {
+            balance = amount;
+        }
+
+        public string GetBalance()
+        {
+            return $"${balance}";
+        }
+
+
+    }
+}
+
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace BankEncapsulation
+﻿using static System.Formats.Asn1.AsnWriter;
+using System;
+
+namespace BankEncapsulation
 {
     public class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+             var kaisAccount = new Bank_Account();
+
+            Console.WriteLine("How much would you like to deposit?");
+
+            var amountToDeposit = double.Parse(Console.ReadLine());
+
+            kaisAccount.Deposit(amountToDeposit);
+
+            Console.WriteLine($"Thank you, your balance is now {kaisAccount.GetBalance()}");
+            
         }
     }
 }
